93a061e6b0d0913052439a58c23f4d92faac932d
Lucas Shao <lucas.shao@foxmail.com>
2018-06-28 17:54:53 +0800

    add routerMiddleware for navigation events via redux actions. that's really cool and more freedom.

--- src/demo-todo-app/components/Footer.tsx
+++ src/demo-todo-app/components/Footer.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
+import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
+import {push, RouterAction} from 'react-router-redux';
+import LocationDescriptor = History.LocationDescriptor;
 
-export default (props) => (
-  <div>
-    <Link to="/">All</Link>
-    <Link to="/active">Active</Link>
-    <Link to="/completed">Completed</Link>
-  </div>
-);
+import {History} from 'history';
+type PropsType = {
+  pushPath: (location: LocationDescriptor, state?: any) => RouterAction;
+};
+
+class Footer extends React.Component<PropsType> {
+  render() {
+    return (
+      <div>
+        <Link to="/">All</Link>
+        <Link to="/active">Active</Link>
+        <Link to="/completed">Completed</Link>
+        <button onClick={() => this.props.pushPath('dash')}>go ?</button>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  (state) => ({}),
+  {pushPath: push}
+)(Footer);
--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
+import {routerMiddleware} from 'react-router-redux';
 import {applyMiddleware, combineReducers, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import messageReducer from '@src/demo-todo-app/reducers/message';
 import todoReducer from '@src/demo-todo-app/reducers/todo';
+import {createBrowserHistory} from 'history';
 
 const reducer = combineReducers({
   todo: todoReducer,
@@ -13,6 +15,9 @@ const reducer = combineReducers({
 export default createStore(
   reducer,
   composeWithDevTools(
-    applyMiddleware(thunk)
+    applyMiddleware(
+      thunk,
+      routerMiddleware(createBrowserHistory())
+    )
   )
 );
