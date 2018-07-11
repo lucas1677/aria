07af968113b540323be67b2a9155df71ce46e900
Lucas Shao <lucas.shao@foxmail.com>
2018-07-11 17:39:33 +0800

    begin to refactor to a normal project.

--- webpack.config.js
+++ webpack.config.js
@@ -9,124 +9,124 @@ const outPath = Path.join(__dirname, './dist');
 const sourcePath = Path.join(__dirname, './src');
 
 module.exports = {
-    mode: isProduction ? "production" : "development",
-    context: sourcePath,
-    entry: {
-        main: './index.tsx',
-        vendor: [
-            'react',
-            'react-dom',
-            'react-redux',
-            'react-router',
-            'redux'
-        ],
-    },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                vendor: {
-                    chunks: "initial",
-                    test: "vendor",
-                    name: "vendor",
-                    enforce: true
-                }
-            }
-        }
-    },
-    output: {
-        path: outPath,
-        publicPath: '/',
-        pathinfo: true
-    },
-    target: "web",
-    resolve: {
-        extensions: ['.ts', '.tsx', '.jsx', '.js'],
-        // Fix webpack's default behavior to not load packages with jsnext:main module
-        // https://github.com/Microsoft/TypeScript/issues/11677
-        mainFields: ['browser', 'main'],
-        alias: {
-            '@src': Path.join(__dirname, 'src')
-        }
-    },
-    module: {
-        rules: [{
-            test: /\.tsx?$/,
-            loader: isProduction
-                ? 'ts-loader'
-                : [
-                    'react-hot-loader/webpack',
-                    'ts-loader'
-                ]
-        }, {
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }, {
-            test: /\.html$/,
-            loader:
-                'html-loader'
-        }, {
-            test: /\.(pdf|jpg|png|gif|svg|ico)$/,
-            use: isProduction
-                ? [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name]-[hash:8].[ext]'
-                        },
-                    },
-                ] : [
-                    {
-                        loader: 'url-loader'
-                    },
-                ]
-        }, {
-            test: /\.(woff|woff2|eot|ttf|otf)$/,
-            use:
-                [
-                    'file-loader'
-                ]
-        }, {
-            test: /\.(csv|tsv)$/,
-            use:
-                [
-                    'csv-loader'
-                ]
-        }, {
-            test: /\.xml$/,
-            use:
-                [
-                    'xml-loader'
-                ]
-        }
-        ],
-    },
-    plugins: isProduction || !isDev ? [
-        new webpack.optimize.AggressiveMergingPlugin(),
-        new HtmlWebpackPlugin({template: 'index.html'})
-    ] : [
-        new webpack.optimize.AggressiveMergingPlugin(),
-        new HtmlWebpackPlugin({template: 'index.html'}),
-        new DashboardPlugin()
+  mode: isProduction ? "production" : "development",
+  context: sourcePath,
+  entry: {
+    main: './index.tsx',
+    vendor: [
+      'react',
+      'react-dom',
+      'react-redux',
+      'react-router',
+      'redux'
     ],
-    devServer:
-        {
-            contentBase: sourcePath,
-            hot:
-                true,
-            stats:
-                {
-                    warnings: false
-                }
-            ,
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: "initial",
+          test: "vendor",
+          name: "vendor",
+          enforce: true
         }
-    ,
-    node: {
-        // workaround for webpack-dev-server issue
-        // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
-        fs: 'empty',
-        net: 'empty'
+      }
     }
+  },
+  output: {
+    path: outPath,
+    publicPath: '/',
+    pathinfo: true
+  },
+  target: "web",
+  resolve: {
+    extensions: ['.ts', '.tsx', '.jsx', '.js'],
+    // Fix webpack's default behavior to not load packages with jsnext:main module
+    // https://github.com/Microsoft/TypeScript/issues/11677
+    mainFields: ['browser', 'main'],
+    alias: {
+      '@src': Path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      loader: isProduction
+        ? 'ts-loader'
+        : [
+          'react-hot-loader/webpack',
+          'ts-loader'
+        ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }, {
+      test: /\.html$/,
+      loader:
+        'html-loader'
+    }, {
+      test: /\.(pdf|jpg|png|gif|svg|ico)$/,
+      use: isProduction
+        ? [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name]-[hash:8].[ext]'
+            },
+          },
+        ] : [
+          {
+            loader: 'url-loader'
+          },
+        ]
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use:
+        [
+          'file-loader'
+        ]
+    }, {
+      test: /\.(csv|tsv)$/,
+      use:
+        [
+          'csv-loader'
+        ]
+    }, {
+      test: /\.xml$/,
+      use:
+        [
+          'xml-loader'
+        ]
+    }
+    ],
+  },
+  plugins: isProduction || !isDev ? [
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new HtmlWebpackPlugin({template: 'index.html'})
+  ] : [
+    new webpack.optimize.AggressiveMergingPlugin(),
+    new HtmlWebpackPlugin({template: 'index.html'}),
+    new DashboardPlugin()
+  ],
+  devServer:
+    {
+      contentBase: sourcePath,
+      hot:
+        true,
+      stats:
+        {
+          warnings: false
+        }
+      ,
+    }
+  ,
+  node: {
+    // workaround for webpack-dev-server issue
+    // https://github.com/webpack/webpack-dev-server/issues/60#issuecomment-103411179
+    fs: 'empty',
+    net: 'empty'
+  }
 };
